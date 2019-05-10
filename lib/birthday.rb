2205a067ddf59572484966efd1f17c769f767c96
Akshat Vishwakarma <aaron.summers2797@gmail.com>
2019-05-10 10:42:10 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -6,7 +6,8 @@ birthday_kids = {
 
 def happy_birthday(birthday_kids)
   birthday_kids.each do |name, age|
-    if age >
+    if age >= 13 
+      puts 
     puts "Happy Birthday #{name}! You are now #{age} years old!"
   end
 end
